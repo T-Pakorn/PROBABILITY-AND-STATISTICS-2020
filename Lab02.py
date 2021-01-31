7f5e797eaec88836fb94cca9178f387cc83cb55b
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-01-31 16:00:14 +0700

    Update Lab02.py

--- Lab02.py
+++ Lab02.py
@@ -1,2 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+
+sample_data = pd.read_csv("August_2018_Nationwide_Airplane_Delay_Statistic.csv")
+plt.plot(sample_data.DEP_DELAY,sample_data.ARR_DELAY)
+plt.show
