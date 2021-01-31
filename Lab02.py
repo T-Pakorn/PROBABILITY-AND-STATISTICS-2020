d742d42dae497db8edbfe695ce8c1498e3aa0acc
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-01-31 16:36:34 +0700

    Update Lab02.py

--- Lab02.py
+++ Lab02.py
@@ -2,5 +2,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 sample_data = pd.read_csv("August_2018_Nationwide_Airplane_Delay_Statistic.csv")
-plt.plot(sample_data.DEP_DELAY,sample_data.ARR_DELAY)
-plt.show
+plt.plot(sample_data.DEP_DELAY,sample_data.ARR_DELAY,'g.')
+plt.title("August 2018 Nationwide Airplane Delay Statistic")
+plt.xlabel("Departure Delay (HHMM)")
+plt.ylabel("Arrival Delay (HHMM)")
+plt.show()
